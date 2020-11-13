a5c6fd0eb312ba47acc4c714db5e271278ab32dc
Synthesizer <65801316+tunlaton11@users.noreply.github.com>
2020-11-13 16:26:10 +0700

    Update show.php

--- show.php
+++ show.php
@@ -18,7 +18,7 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table class="table table-dark table-striped" width="600" border="1">
+<table class="table table-dark table-striped", width="600" border="1">
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
