46677a3fdb633a6586fd1201e383abf707af5278
Synthesizer <65801316+tunlaton11@users.noreply.github.com>
2020-11-13 17:10:02 +0700

    Update show.php

--- show.php
+++ show.php
@@ -43,7 +43,7 @@ mysqli_close($conn);
 <div class="container">
   <div class="row">
     <div class="col text-center">
-      <a href="https://itf-224-db.azurewebsites.net/form.html" class="btn btn-info" role="button">Click here</a>
+      <a href="https://itf-224-db.azurewebsites.net/form.html" class="btn btn-info" role="button">Insert Data</a>
     </div>
   </div>
 </div>
