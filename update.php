a3b963568d3ddc3a9eeeeaf62695476228cbf6db
Synthesizer <65801316+tunlaton11@users.noreply.github.com>
2020-11-17 17:25:35 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ITF Database Lab</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'itf-224-db-lab.mysql.database.azure.com', 'tunlaton11@itf-224-db-lab', 'Kaozaa0089', 'itflab', 3306);
@@ -23,15 +13,11 @@ $link = $_POST['link'];
 $sql = "UPDATE guestbook SET Name='$name', Comment='$text', Link='$link' WHERE ID='$ID'";
 
 if (mysqli_query($conn, $sql)) {
-    echo '<div class="container">
-            <h3>Comment has been updated successfully.</h3>
-            <a role="button" class="btn btn-primary mt-3" href="index.php">Home</a>
-         </div>';
-} else {
+    echo "Edit successfully!!    Check updated -->>";
+    echo '<a href="https://itf-224-db.azurewebsites.net/index.php">CLICK HERE</a>';
+  } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+  }
 
 mysqli_close($conn);
 ?>
-</body>
-</html>
